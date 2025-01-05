d18cdc22c98dad7aa916f1328b1c72607cae2b6f
TechPeriod <77057831+tech-period@users.noreply.github.com>
2025-01-05 22:30:40 +0900

    ルーティングの例外設定を追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 
 export const routes: Routes = [
-  { path: '', component: HomepageComponent }
+  { path: '', component: HomepageComponent },
+  { path: '**', redirectTo: '' }
 ];
