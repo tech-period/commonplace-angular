6d45e8db730573420a9f02573b4c72ef8929a09d
TechPeriod <77057831+tech-period@users.noreply.github.com>
2025-01-05 22:32:59 +0900

    ナビゲーションリストコンポーネントをサイドナビゲーションに配置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
+import { NavListComponent } from './components/nav-list/nav-list.component';
+
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     MatSidenavModule,
     NavbarComponent,
+    NavListComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
