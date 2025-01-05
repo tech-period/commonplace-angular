642b90513418a7521f103589a859ee2765ee9d78
TechPeriod <77057831+tech-period@users.noreply.github.com>
2025-01-05 22:41:04 +0900

    サイズに応じてサイドナビゲーションの表示を切り替えるように修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subscription } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 import { NavListComponent } from './components/nav-list/nav-list.component';
 
@@ -19,7 +22,27 @@ import { NavListComponent } from './components/nav-list/nav-list.component';
 export class AppComponent {
 
   isSidenavOpened: boolean = false;
+  private breakpointStateSub$: Subscription;
   constructor(
+    private breakpointObserver: BreakpointObserver,
   ) {
+    this.breakpointStateSub$ = this.breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+      ])
+      .pipe(
+        distinctUntilChanged((prev, curr) =>
+          prev.matches === curr.matches
+        ),
+      )
+      .subscribe((state) => {
+        console.log('state', state);
+        this.isSidenavOpened = state.matches;
+      });
+  }
+
+  ngOnDestroy() {
+    this.breakpointStateSub$.unsubscribe();
   }
 }
