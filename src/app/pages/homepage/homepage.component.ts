1929828b1d221f5262efc89fa415ef44f9e3d79f
TechPeriod <77057831+tech-period@users.noreply.github.com>
2025-01-10 21:39:16 +0900

    ホームページコンポーネント内に概要用のMatCardコンポーネントを配置

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [
+    MatCardModule
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
   title = 'commonplace-angular';
+
+  cardTitles:string[] = [
+    'sample1',
+    'sample2',
+    'sample3',
+  ];
 }
