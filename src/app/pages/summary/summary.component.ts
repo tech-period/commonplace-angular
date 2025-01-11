9154ca17dbe23fc165ae552f666ab79d09b3dd8a
TechPeriod <77057831+tech-period@users.noreply.github.com>
2025-01-11 22:54:39 +0900

    サマリーページにtableコンポーネントを配置

--- src/app/pages/summary/summary.component.ts
+++ src/app/pages/summary/summary.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TableComponent } from '../../components/table/table.component';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  imports: [
+    TableComponent
+  ],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
