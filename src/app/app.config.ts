30611c99eaddfb314961c573ce593ff05e21c042
TechPeriod <77057831+tech-period@users.noreply.github.com>
2025-01-23 22:53:40 +0900

    テスト用の共通設定を実装

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
@@ -18,3 +20,19 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
   ]
 };
+
+// テスト用共通設定
+export const configureTestBed = () => {
+  TestBed.configureTestingModule({
+    imports: [
+      LayoutModule,
+      OverlayModule,
+    ],
+    providers: [
+      provideZoneChangeDetection({ eventCoalescing: true }),
+      provideRouter(routes),
+      provideAnimations(),
+      provideStore(),
+    ]
+  });
+}
