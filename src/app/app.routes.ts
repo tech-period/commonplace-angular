35b4ddae20dab98f9ff9c8fc4ec286aa0ae3bdc4
TechPeriod <77057831+tech-period@users.noreply.github.com>
2025-01-25 23:48:26 +0900

    認証ガードを実装

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './guards/auth.guard';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { SummaryComponent } from './pages/summary/summary.component';
-import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'summary', component: SummaryComponent },
+  { path: 'summary', component: SummaryComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
 ];
