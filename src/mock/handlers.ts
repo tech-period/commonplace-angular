591e73a7f349cefb9a2fe34455556eb46bbae129
TechPeriod <77057831+tech-period@users.noreply.github.com>
2025-01-28 22:19:19 +0900

    モックAPIにPOSTメソッドを追加

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -6,4 +6,17 @@ export const handlers = [
       message: 'モックレスポンス'
     });    
   }),
+
+  http.post('/api/example', async ({ request }) => {
+    const body = await request.json();
+    if(Object.keys(body as object).length != 0) {
+      return HttpResponse.json({
+        message: 'モックレスポンス',
+      }, { status: 200 });
+    } else {
+      return HttpResponse.json({
+        message: 'リクエストボディが空です',
+      }, { status: 400 });
+    }
+  }),
 ];
